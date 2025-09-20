440fa6fe43b8d6a946ea88b304fd189587de0b12
kulakov444 <kulakov444@noreply.codeberg.org>
2025-09-20 18:39:25 +0700

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "name.kulakov444.wadbswitch"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
